6f5dac78bae3627e0c176036f780445c23ee7593
User <girain3181@gmail.com>
2023-10-07 00:29:09 +0300

    finish

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -4,8 +4,9 @@ config();
 
 const configs = {
   SALT: 10,
-  PORT: process.env.PORT || 5200,
+  PORT: 5000,
   DB_URL: process.env.DB_URL || "mongodb://localhost:27017/march-2023",
+
   SYCRET_ACCESS: process.env.SYCRET_ACCESS || "",
   SYCRET_REFRESH: process.env.SYCRET_REFRESH || "",
 };
