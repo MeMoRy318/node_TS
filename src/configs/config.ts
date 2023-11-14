20db5a8a658810eea8476ad3da028b0e3eda8948
User <girain3181@gmail.com>
2023-11-14 12:19:13 +0300

    finish

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -14,9 +14,6 @@ const configs = {
   JWT_SECRET_FORGOT: process.env.JWT_SECRET_FORGOT || "",
   JWT_SECRET_ACTIVATE: process.env.JWT_SECRET_ACTIVATE || "",
 
-  NODEMAILER_EMAIL: process.env.NO_REPLY_EMAIL || "example@gmail.com",
-  NODEMAILER_PASSWORD: process.env.NO_REPLY_PASSWORD || "************",
-
   AWS_S3_ACCESS_KEY: process.env.AWS_S3_ACCESS_KEY,
   AWS_S3_SECRET_KEY: process.env.AWS_S3_SECRET_KEY,
   AWS_S3_BUKET: process.env.AWS_S3_BUKET,
